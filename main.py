65c6004c5b9e65c936f14ef6460038db443e97cf
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-06-27 14:55:33 +0530

    Bug fix

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ print("Welcome to the tip calculator!")
 bill = input("What was the total bill? $ ")
 tip = input("How much tip would you like to give? 10, 12, or 15?  ")
 people = input("How many people to split the bill? ")
-f_bill = str((int(bill) + int(tip))/int(people))
-
+f_bill = str(round((int(bill) + int(tip))/int(people), 2))
 print(f"Each person should pay: ${f_bill}")
 print("Thanks for Using")
